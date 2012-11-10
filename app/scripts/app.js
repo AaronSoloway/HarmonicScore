6c2d98b497d1647514c0bde609ab46c88a95d5ef
X <X@Xs-MacBook-Pro.local>
2012-11-09 21:32:13 -0500

    Added Raphael paper OnLoad and put a kind of hack place holder to look like notes visualized.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,40 @@
 define([], function() {
   function App() {
+    this.OnLoad = function(){
+      console.log("hey");
+
+      //define progromatically based on window size
+      var x = 300; 
+      var y = 205; 
+      var width = 850; 
+      var height = 200; 
+
+      // created Raphael paper on which to draw
+      var paper = new Raphael(x, y, width, height); //option (a) 
+      
+      // color background so you know where the canvas is
+      var placemat = paper.rect(0,0,paper.width,paper.height).attr({"fill" : "#333333" });
+      
+      // taken from http://jsdo.it/remmel/1qGu
+      // roundedRectangle(x, y, width, height, upper_left_corner, upper_right_corner, lower_right_corner, lower_left_corner)
+      Raphael.fn.roundedRectangle = function (x, y, w, h, r1, r2, r3, r4){
+        var array = [];
+        array = array.concat(["M",x,r1+y, "Q",x,y, x+r1,y]); //A
+        array = array.concat(["L",x+w-r2,y, "Q",x+w,y, x+w,y+r2]); //B
+        array = array.concat(["L",x+w,y+h-r3, "Q",x+w,y+h, x+w-r3,y+h]); //C
+        array = array.concat(["L",x+r4,y+h, "Q",x,y+h, x,y+h-r4, "Z"]); //D
+
+        return this.path(array);
+      };
+
+      //Pretend!  This is what a few nots might look like sans harmonics
+      paper.roundedRectangle(10, 10, 80, 5, 1, 1, 1, 1).attr({fill: "#f00"});
+      paper.roundedRectangle(100, 150, 80, 5, 1, 1, 1, 1).attr({fill: "#f00"});
+      paper.roundedRectangle(200, 100, 80, 5, 1, 1, 1, 1).attr({fill: "#f00"});
+      paper.roundedRectangle(500, 70, 80, 5, 1, 1, 1, 1).attr({fill: "#f00"});
+      
+      console.log("loaded");
+    }
     this.MidiChanged = function(data){
       this.ClearEvents();
       var time = 0;
