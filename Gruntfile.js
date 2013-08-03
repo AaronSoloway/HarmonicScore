d4ffa2535580f6c4b74e6b798041c4fb895ce3bd
russellmcc <russell.mcclellan@gmail.com>
2013-08-03 17:09:19 -0400

    WTF yeoman.  bad call.

--- Gruntfile.js
+++ Gruntfile.js
@@ -241,22 +241,6 @@ module.exports = function (grunt) {
                 }]
             }
         },
-        cssmin: {
-            // This task is pre-configured if you do not wish to use Usemin
-            // blocks for your CSS. By default, the Usemin block from your
-            // `index.html` will take care of minification, e.g.
-            //
-            //     <!-- build:css({.tmp,app}) styles/main.css -->
-            //
-            // dist: {
-            //     files: {
-            //         '<%= yeoman.dist %>/styles/main.css': [
-            //             '.tmp/styles/{,*/}*.css',
-            //             '<%= yeoman.app %>/styles/{,*/}*.css'
-            //         ]
-            //     }
-            // }
-        },
         htmlmin: {
             dist: {
                 options: {
@@ -358,7 +342,6 @@ module.exports = function (grunt) {
         'concurrent:dist',
         'requirejs',
         'concat',
-        'cssmin',
         'uglify',
         'copy:dist',
         'rev',
